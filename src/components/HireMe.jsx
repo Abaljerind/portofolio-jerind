dfc2d2c5ce316efba4bd8b3c7a785dbdc4055c57
Abaljerind <abaljerinda@gmail.com>
2025-02-17 21:49:06 +0700

    Fix width of wrapper

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function HireMe() {
   return (
-    <div className="flex h-[47px] w-[100px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB]">
+    <div className="flex h-[47px] w-[98px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB]">
       <Link to="/contact">
         <button className="flex cursor-pointer items-center rounded-md bg-[#141414] p-2 text-xs font-bold text-white">
           <img src="../images/Add.svg" alt="Add Icon" className="size-5" />
