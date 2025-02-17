27af8d6f698023d4bd00ddc61637d51d6221fd05
Abaljerind <abaljerinda@gmail.com>
2025-02-17 21:00:06 +0700

    Wrap tag Link with div to create effect for when user hover it

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function HireMe() {
   return (
-    <Link to="/contact">
-      <button className="flex cursor-pointer items-center rounded-md bg-[#141414] p-2 text-xs font-bold text-white">
-        <img src="../images/Add.svg" alt="Add Icon" className="size-5" />
-        <span className="pl-1">Hire Me</span>
-      </button>
-    </Link>
+    <div className="flex h-[47px] w-[100px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB]">
+      <Link to="/contact">
+        <button className="flex cursor-pointer items-center rounded-md bg-[#141414] p-2 text-xs font-bold text-white">
+          <img src="../images/Add.svg" alt="Add Icon" className="size-5" />
+          <span className="pl-1">Hire Me</span>
+        </button>
+      </Link>
+    </div>
   );
 }
