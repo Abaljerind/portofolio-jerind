f9d669c4258c3fac4d23be0b6b1e999cc067a210
Abaljerind <abaljerinda@gmail.com>
2025-02-17 18:13:25 +0700

    Create GreenCircle component and give it styles

---
+++ src/components/GreenCircle.jsx
@@ -0,0 +1,7 @@
+export default function GreenCircle() {
+  return (
+    <div className="outer-circle flex h-6 w-6 items-center justify-center rounded-full bg-[#DFF7EC]">
+      <div className="inner-circle h-1.5 w-1.5 rounded-full bg-[#28C780]"></div>
+    </div>
+  );
+}
