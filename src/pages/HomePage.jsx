6503e75785d72d7d6927f76b0a09b51105630093
Abaljerind <abaljerinda@gmail.com>
2025-02-18 17:59:33 +0700

    Add content of page HomePage and give it styles
    Invoke component GrayCircle, GreenCircle, Buttons, ViewAll, ProjectCards, ContactSection and Footer
    Create header section
    Create biodata section
    Create profile section
    Create projects section with title-project and use component ProjectCards
    Use component ContactSection and Footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,71 @@
+import GrayCircle from "../components/GrayCircle";
+import GreenCircle from "../components/GreenCircle";
+import Buttons from "../components/Buttons";
+import ViewAll from "../components/ViewAll";
+import ProjectCards from "../components/ProjectCards";
+import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="mt-2 rounded-2xl bg-white py-2 shadow-sm">
+      {/* header */}
+      <div className="title-biodata flex items-center justify-between px-6 py-5">
+        <div className="flex items-center justify-between">
+          <GrayCircle />
+          <p className="text-lg font-semibold text-[#666666]">
+            Frontend Developer
+          </p>
+        </div>
+        <GreenCircle />
+      </div>
+
+      <div className="biodata mt-2">
+        <div className="selfie flex items-center justify-center">
+          <div className="outer-circle flex h-[165px] w-[165px] items-center justify-center rounded-full bg-[#F7F7F7]">
+            <img
+              src="../images/avataaars.png"
+              alt="avatar image"
+              className="h-36 w-36 rounded-full bg-[#D8D8DA]"
+            />
+          </div>
+        </div>
+
+        <div className="profile my-6 flex flex-col items-center justify-center">
+          <p className="mb-4 text-3xl font-bold text-[#373737]">
+            I'm AbalJerind
+          </p>
+          <p className="px-3 text-center text-[16px] leading-7 font-medium break-words text-[#6D6D6D]">
+            Frontend developer from Jakarta, ID. Currently building interfaces
+            at Home.
+          </p>
+        </div>
+
+        <Buttons />
+      </div>
+
+      {/* projects */}
+      <div className="mt-12 px-2">
+        <div className="rounded-xl bg-[#F7F7F7] px-2 py-4">
+          <div className="title-project flex justify-between">
+            <div className="flex items-center justify-center pl-2">
+              <GrayCircle />
+              <p className="text-lg font-semibold text-[#666666]">Projects</p>
+            </div>
+            <ViewAll />
+          </div>
+
+          <ProjectCards />
+        </div>
+      </div>
+
+      {/* contact */}
+      <ContactSection />
+
+      {/* footer */}
+      <Footer />
+    </div>
+  );
 }
 
 export default HomePage;
