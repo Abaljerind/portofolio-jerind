da1dff2c12857f9f387e6fa045c0d3279d56445b
Abaljerind <abaljerinda@gmail.com>
2025-02-18 18:14:42 +0700

    Edit tag p to be h3

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,9 +4,9 @@ export default function ContactSection() {
   return (
     <div className="mt-12 px-2">
       <div className="texts mb-4 flex flex-col items-center justify-center text-center">
-        <p className="mb-4 text-3xl font-bold text-[#373737]">
+        <h3 className="mb-4 text-3xl font-bold text-[#373737]">
           Let's work together.
-        </p>
+        </h3>
         <small className="px-8 text-[16px] leading-7 font-medium break-words text-[#777777]">
           Crafting modern and user-friendly web interfaces
         </small>
