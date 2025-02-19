f4fa4f658511d5016f7011551c1258a10159c017
Abaljerind <abaljerinda@gmail.com>
2025-02-19 16:36:05 +0700

    Create component MySkills and give it styles
    Receive props from page About

---
+++ src/components/MySkills.jsx
@@ -0,0 +1,7 @@
+export default function MySkills({ skill }) {
+  return (
+    <div className="rounded-full bg-white px-5 py-2 shadow-sm">
+      <p className="text-sm font-semibold text-[#393939]">{skill}</p>
+    </div>
+  );
+}
