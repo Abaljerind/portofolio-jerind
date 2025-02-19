13998360f1186c68720a1ca204d85a4447357875
Abaljerind <abaljerinda@gmail.com>
2025-02-19 17:43:32 +0700

    Create content of page Project and give it styles
    Invoke component GrayCircle, ProjectCards, ContactSection and Footer
    Create Project header and introduction of page Project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,5 +1,36 @@
+import ContactSection from "../components/ContactSection";
+import GrayCircle from "../components/GrayCircle";
+import ProjectCards from "../components/ProjectCards";
+import Footer from "../components/Footer";
+
 function Project() {
-  return <h1>Project Page</h1>;
+  return (
+    <>
+      <div className="title-about flex items-center justify-between px-6 py-5">
+        <div className="flex items-center justify-between">
+          <GrayCircle />
+          <p className="text-lg font-semibold text-[#666666]">Projects</p>
+        </div>
+      </div>
+
+      <div className="my-projects flex flex-col px-6">
+        <h2 className="mb-4 text-3xl font-bold text-[#373737]">My Works</h2>
+        <p className="mb-10 text-justify text-[16px] leading-7 tracking-tight text-[#666666]">
+          Explore my portfolio, where clean code meets seamless design. I craft
+          responsive and user-friendly web experiences that engage and inspire.
+        </p>
+      </div>
+
+      <div className="p-2">
+        <div className="rounded-xl bg-[#F7F7F7] p-2">
+          <ProjectCards />
+        </div>
+
+        <ContactSection />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default Project;
