f46f28e1a555303601372e2d49159d16f7d5ecbb
Abaljerind <abaljerinda@gmail.com>
2025-02-21 18:12:41 +0700

    Add dark variant for tag div
    Add dark variant for background color

--- src/components/GreenCircle.jsx
+++ src/components/GreenCircle.jsx
@@ -1,6 +1,6 @@
 export default function GreenCircle() {
   return (
-    <div className="outer-circle flex h-6 w-6 items-center justify-center rounded-full bg-[#DFF7EC]">
+    <div className="outer-circle flex h-6 w-6 items-center justify-center rounded-full bg-[#DFF7EC] dark:bg-[#223A2F]">
       <div className="inner-circle h-1.5 w-1.5 rounded-full bg-[#28C780]"></div>
     </div>
   );
