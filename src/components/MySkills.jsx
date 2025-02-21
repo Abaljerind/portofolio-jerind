0bbe01e7fc54d04dd1cc1ad1ced61997c80c7946
Abaljerind <abaljerinda@gmail.com>
2025-02-21 18:21:21 +0700

    Add dark variant for tag div and p
    Add dark variant for background color and text color

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,7 +1,9 @@
 export default function MySkills({ skill }) {
   return (
-    <div className="rounded-full bg-white px-5 py-2 shadow-sm">
-      <p className="text-sm font-semibold text-[#393939]">{skill}</p>
+    <div className="rounded-full bg-white px-5 py-2 shadow-sm dark:bg-[#373737]">
+      <p className="text-sm font-semibold text-[#393939] dark:text-white">
+        {skill}
+      </p>
     </div>
   );
 }
