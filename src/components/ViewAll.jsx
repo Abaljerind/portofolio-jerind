6c06977aa48a2a5ba47b7ac573399ba436d71c63
Abaljerind <abaljerinda@gmail.com>
2025-02-21 17:43:06 +0700

    Add dark variant for tag Link, button, span and img
    Add dark variant for background color, border, text color, brightness and invert

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -4,14 +4,16 @@ export default function ViewAll() {
   return (
     <Link
       to={"/project"}
-      className="flex h-[46px] w-[101px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB]"
+      className="flex h-[46px] w-[101px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB] dark:hover:bg-[#1C1C1C]"
     >
-      <button className="flex scale-90 cursor-pointer items-center justify-between rounded-lg border-2 border-[#EBEBEB] bg-white p-2.5">
-        <span className="mr-2 text-sm font-bold text-[#373737]">View All</span>
+      <button className="flex scale-90 cursor-pointer items-center justify-between rounded-lg border-2 border-[#EBEBEB] bg-white p-2.5 dark:border-none dark:bg-[#373737]">
+        <span className="mr-2 text-sm font-bold text-[#373737] dark:text-white">
+          View All
+        </span>
         <img
           src="../images/RightArrow.svg"
           alt="icon right arrow"
-          className="size-[14px] scale-75"
+          className="size-[14px] scale-75 dark:brightness-0 dark:invert"
         />
       </button>
     </Link>
