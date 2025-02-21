7c2405cf57f56f8801ec987febfd262b45d7279c
Abaljerind <abaljerinda@gmail.com>
2025-02-21 17:27:21 +0700

    Add dark variant for tag h3 and small
    add dark variant for text color

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,10 +4,10 @@ export default function ContactSection() {
   return (
     <div className="mt-12 px-2">
       <div className="texts mb-4 flex flex-col items-center justify-center text-center">
-        <h3 className="mb-4 text-3xl font-bold text-[#373737]">
+        <h3 className="mb-4 text-3xl font-bold text-[#373737] dark:text-white">
           Let's work together.
         </h3>
-        <small className="px-8 text-[16px] leading-7 font-medium break-words text-[#777777]">
+        <small className="px-8 text-[16px] leading-7 font-medium break-words text-[#777777] dark:text-[#B7B7B7]">
           Crafting modern and user-friendly web interfaces
         </small>
       </div>
