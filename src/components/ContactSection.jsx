712a08f28613f1420f8caa149794f96beac1f219
Abaljerind <abaljerinda@gmail.com>
2025-02-22 07:46:27 +0700

    Add styles for breakpoint md for h3 and small
    Add styles for text size

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,10 +4,10 @@ export default function ContactSection() {
   return (
     <div className="mt-12 px-2">
       <div className="texts mb-4 flex flex-col items-center justify-center text-center">
-        <h3 className="mb-4 text-3xl font-bold text-[#373737] dark:text-white">
+        <h3 className="mb-4 text-3xl font-bold text-[#373737] md:text-4xl dark:text-white">
           Let's work together.
         </h3>
-        <small className="px-8 text-[16px] leading-7 font-medium break-words text-[#777777] dark:text-[#B7B7B7]">
+        <small className="px-8 text-[16px] leading-7 font-medium break-words text-[#777777] md:text-lg dark:text-[#B7B7B7]">
           Crafting modern and user-friendly web interfaces
         </small>
       </div>
