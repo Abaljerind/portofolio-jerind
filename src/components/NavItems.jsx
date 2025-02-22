47c5e5ebb258eb24c7108185d0d8668ffe67aafe
Abaljerind <abaljerinda@gmail.com>
2025-02-22 20:38:58 +0700

    Add styles for breakpoint lg for ul
    Add styles for gap

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -10,7 +10,7 @@ export default function NavItems() {
   ];
 
   return (
-    <ul className="flex justify-between md:justify-start md:gap-6">
+    <ul className="flex justify-between md:justify-start md:gap-6 lg:gap-8">
       {menuItems.map((nav, index) => {
         return (
           <li className="group relative cursor-pointer" key={index + 1}>
