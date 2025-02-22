0039a133b57bfd5498265a7ad848649a5d7c73ec
Abaljerind <abaljerinda@gmail.com>
2025-02-22 20:46:23 +0700

    Add div for wrap title-contact, intro-contacts and form
    Add styles for breakpoint lg for padding left and right

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -43,88 +43,90 @@ function Contact() {
 
   return (
     <>
-      <div className="title-contact flex items-center justify-between px-6 py-5">
-        <div className="flex items-center justify-between">
-          <GrayCircle />
-          <p className="text-lg font-semibold text-[#666666] md:text-xl dark:text-[#C0C0C0]">
-            Hire Me
+      <div className="lg:px-2">
+        <div className="title-contact flex items-center justify-between px-6 py-5">
+          <div className="flex items-center justify-between">
+            <GrayCircle />
+            <p className="text-lg font-semibold text-[#666666] md:text-xl dark:text-[#C0C0C0]">
+              Hire Me
+            </p>
+          </div>
+          <GreenCircle />
+        </div>
+
+        <div className="intro-contacts mt-4 flex flex-col px-6">
+          <h2 className="mb-4 text-3xl font-bold text-[#373737] md:text-4xl dark:text-white">
+            Frontend Request
+          </h2>
+          <p className="mb-8 text-justify text-[16px] leading-7 tracking-tight text-[#666666] md:text-lg dark:text-[#C0C0C0]">
+            Have an idea and need a developer to bring your design to life?
+            Reach out now
           </p>
         </div>
-        <GreenCircle />
-      </div>
 
-      <div className="intro-contacts mt-4 flex flex-col px-6">
-        <h2 className="mb-4 text-3xl font-bold text-[#373737] md:text-4xl dark:text-white">
-          Frontend Request
-        </h2>
-        <p className="mb-8 text-justify text-[16px] leading-7 tracking-tight text-[#666666] md:text-lg dark:text-[#C0C0C0]">
-          Have an idea and need a developer to bring your design to life? Reach
-          out now
-        </p>
-      </div>
-
-      <div className="form px-6">
-        <form
-          action=""
-          onSubmit={handleForSubmit}
-          className="flex flex-col gap-3"
-        >
-          <div className="flex w-full gap-3">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Name"
-              className="w-[50%] rounded-lg border border-[#2c2c2c]/10 bg-[#F7F7F7] p-3 text-sm font-medium text-[#515151] placeholder:text-sm placeholder:font-medium placeholder:text-[#A0A0A0] focus:border-[#2c2c2c]/50 focus:ring-0 focus:outline-0 md:p-4 dark:bg-[#2C2C2C]"
-              value={formData.name}
-              onChange={handleChange}
-              required
-              autoComplete="off"
-            />
-            <ValidationError
-              prefix="Name"
-              field="name"
-              errors={submitData.errors}
-            />
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email Address"
-              className="font-mediumplaceholder:font-medium w-[50%] rounded-lg border border-[#2c2c2c]/10 bg-[#F7F7F7] p-3 text-sm text-[#515151] placeholder:text-sm placeholder:text-[#A0A0A0] focus:border-[#2c2c2c]/50 focus:ring-0 focus:outline-0 dark:bg-[#2C2C2C]"
-              required
-              value={formData.email}
-              onChange={handleChange}
-              autoComplete="off"
-            />
-            <ValidationError
-              prefix="Email"
-              field="email"
-              errors={submitData.errors}
-            />
-          </div>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Message"
-            className="h-36 rounded-lg border border-[#2c2c2c]/10 bg-[#F7F7F7] p-3 text-sm font-medium text-[#515151] placeholder:text-sm placeholder:font-medium placeholder:text-[#A0A0A0] focus:border-[#2c2c2c]/50 focus:ring-0 focus:outline-0 dark:bg-[#2C2C2C]"
-            required
-            value={formData.message}
-            onChange={handleChange}
-          ></textarea>
-          <ValidationError
-            prefix="Message"
-            field="message"
-            errors={submitData.errors}
-          />
-
-          <button
-            type="submit"
-            className="w-full cursor-pointer rounded-lg bg-black py-3 text-sm font-bold text-white duration-500 hover:opacity-80 dark:bg-[#383838] dark:hover:bg-[#424242]"
+        <div className="form px-6">
+          <form
+            action=""
+            onSubmit={handleForSubmit}
+            className="flex flex-col gap-3"
           >
-            Submit
-          </button>
-        </form>
+            <div className="flex w-full gap-3">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Name"
+                className="w-[50%] rounded-lg border border-[#2c2c2c]/10 bg-[#F7F7F7] p-3 text-sm font-medium text-[#515151] placeholder:text-sm placeholder:font-medium placeholder:text-[#A0A0A0] focus:border-[#2c2c2c]/50 focus:ring-0 focus:outline-0 md:p-4 dark:bg-[#2C2C2C]"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                autoComplete="off"
+              />
+              <ValidationError
+                prefix="Name"
+                field="name"
+                errors={submitData.errors}
+              />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Address"
+                className="font-mediumplaceholder:font-medium w-[50%] rounded-lg border border-[#2c2c2c]/10 bg-[#F7F7F7] p-3 text-sm text-[#515151] placeholder:text-sm placeholder:text-[#A0A0A0] focus:border-[#2c2c2c]/50 focus:ring-0 focus:outline-0 dark:bg-[#2C2C2C]"
+                required
+                value={formData.email}
+                onChange={handleChange}
+                autoComplete="off"
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={submitData.errors}
+              />
+            </div>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Message"
+              className="h-36 rounded-lg border border-[#2c2c2c]/10 bg-[#F7F7F7] p-3 text-sm font-medium text-[#515151] placeholder:text-sm placeholder:font-medium placeholder:text-[#A0A0A0] focus:border-[#2c2c2c]/50 focus:ring-0 focus:outline-0 dark:bg-[#2C2C2C]"
+              required
+              value={formData.message}
+              onChange={handleChange}
+            ></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={submitData.errors}
+            />
+
+            <button
+              type="submit"
+              className="w-full cursor-pointer rounded-lg bg-black py-3 text-sm font-bold text-white duration-500 hover:opacity-80 dark:bg-[#383838] dark:hover:bg-[#424242]"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
 
       <Footer />
