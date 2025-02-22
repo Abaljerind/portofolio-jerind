49368ebdccbe5b66686692867a5114008c57f4e5
Abaljerind <abaljerinda@gmail.com>
2025-02-22 07:59:58 +0700

    Add styles for breakpoint md for tag button
    Add styles for text size

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -4,7 +4,7 @@ export default function HireMe() {
   return (
     <div className="flex h-[47px] w-[98px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB] dark:hover:bg-[#1C1C1C]">
       <Link to="/contact">
-        <button className="flex cursor-pointer items-center rounded-md bg-[#141414] p-2 text-xs font-bold text-white dark:bg-[#383838]">
+        <button className="flex cursor-pointer items-center rounded-md bg-[#141414] p-2 text-xs font-bold text-white md:text-sm dark:bg-[#383838]">
           <img src="../images/Add.svg" alt="Add Icon" className="size-5" />
           <span className="pl-1">Hire Me</span>
         </button>
