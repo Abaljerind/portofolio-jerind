de8643022cf587b5be6a70eadca7601d1cea4488
Abaljerind <abaljerinda@gmail.com>
2025-02-22 17:12:41 +0700

    Add styles for breakpoint md for p
    Add styles for text size

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,7 +1,7 @@
 export default function MySkills({ skill }) {
   return (
     <div className="rounded-full bg-white px-5 py-2 shadow-sm dark:bg-[#373737]">
-      <p className="text-sm font-semibold text-[#393939] dark:text-white">
+      <p className="text-sm font-semibold text-[#393939] md:text-[16px] dark:text-white">
         {skill}
       </p>
     </div>
