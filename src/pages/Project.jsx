6769b3b4f1a3f646b5ddc9effc886d7c5ed05079
Abaljerind <abaljerinda@gmail.com>
2025-02-22 17:33:45 +0700

    Add styles for breakpoint md for p and h2
    Add styles for text size, margin top and bottom

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -9,17 +9,17 @@ function Project() {
       <div className="title-about flex items-center justify-between px-6 py-5">
         <div className="flex items-center justify-between">
           <GrayCircle />
-          <p className="text-lg font-semibold text-[#666666] dark:text-[#C0C0C0]">
+          <p className="text-lg font-semibold text-[#666666] md:text-xl dark:text-[#C0C0C0]">
             Projects
           </p>
         </div>
       </div>
 
       <div className="my-projects flex flex-col px-6">
-        <h2 className="mb-4 text-3xl font-bold text-[#373737] dark:text-white">
+        <h2 className="mb-4 text-3xl font-bold text-[#373737] md:mt-4 md:mb-6 md:text-4xl dark:text-white">
           My Works
         </h2>
-        <p className="mb-10 text-justify text-[16px] leading-7 tracking-tight text-[#666666] dark:text-[#C0C0C0]">
+        <p className="mb-10 text-justify text-[16px] leading-7 tracking-tight text-[#666666] md:text-lg dark:text-[#C0C0C0]">
           Explore my portfolio, where clean code meets seamless design. I craft
           responsive and user-friendly web experiences that engage and inspire.
         </p>
