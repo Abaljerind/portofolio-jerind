703a575f09a052ebac1abb55d2db8aa39c45b899
Abaljerind <abaljerinda@gmail.com>
2025-02-22 08:03:07 +0700

    Add styles for breakpoint md for tag button
    Add styles for text size

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -6,7 +6,7 @@ export default function ViewAll() {
       to={"/project"}
       className="flex h-[46px] w-[101px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB] dark:hover:bg-[#1C1C1C]"
     >
-      <button className="flex scale-90 cursor-pointer items-center justify-between rounded-lg border-2 border-[#EBEBEB] bg-white p-2.5 dark:border-none dark:bg-[#373737]">
+      <button className="flex w-fit scale-90 cursor-pointer items-center justify-between rounded-lg border-2 border-[#EBEBEB] bg-white p-2.5 md:text-lg dark:border-none dark:bg-[#373737]">
         <span className="mr-2 text-sm font-bold text-[#373737] dark:text-white">
           View All
         </span>
