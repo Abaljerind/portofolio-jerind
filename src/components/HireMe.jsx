0d0a3f01fccae8c7504d545f52245e41e5973311
Abaljerind <abaljerinda@gmail.com>
2025-02-22 18:11:07 +0700

    Add styles for breakpoint md for div
    Add styles for width and height

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function HireMe() {
   return (
-    <div className="flex h-[47px] w-[98px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB] dark:hover:bg-[#1C1C1C]">
+    <div className="flex h-[47px] w-[98px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB] md:h-[52px] md:w-[108px] dark:hover:bg-[#1C1C1C]">
       <Link to="/contact">
         <button className="flex cursor-pointer items-center rounded-md bg-[#141414] p-2 text-xs font-bold text-white md:text-sm dark:bg-[#383838]">
           <img src="../images/Add.svg" alt="Add Icon" className="size-5" />
