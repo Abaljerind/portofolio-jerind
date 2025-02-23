c89052f1ea2ddd34e659cf81bb48f1c1c0c9ec9b
Abaljerind <abaljerinda@gmail.com>
2025-02-23 08:54:21 +0700

    Add max width and change width size for Link, delete mr-2 for span

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -4,10 +4,10 @@ export default function ViewAll() {
   return (
     <Link
       to={"/project"}
-      className="flex h-[46px] w-[101px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB] dark:hover:bg-[#1C1C1C]"
+      className="flex h-[46px] w-[104px] max-w-[108px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB] dark:hover:bg-[#1C1C1C]"
     >
       <button className="flex w-full scale-90 cursor-pointer items-center justify-between rounded-lg border-2 border-[#EBEBEB] bg-white p-2.5 md:text-lg dark:border-none dark:bg-[#373737]">
-        <span className="mr-2 text-sm font-bold text-[#373737] dark:text-white">
+        <span className="text-sm font-bold text-[#373737] dark:text-white">
           View All
         </span>
         <img
