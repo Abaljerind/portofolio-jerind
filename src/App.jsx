b9b7bdb797da2422d948a99794018e0ca2127b3e
AbalJerind <abaljerinda@gmail.com>
2025-09-01 15:31:43 +0700

    Fix(App): Re-adjust width for breakpoint lg and min width 769px

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   // kiri kanan didalam navbar, ujung kiri kanannya 16px
 
   return (
-    <div className="font-openSans p-2 md:mx-auto md:w-full md:max-w-[576px] lg:pt-4 lg:pb-12">
+    <div className="font-openSans p-2 md:mx-auto min-[769px]:max-w-[510px] min-[769px]:mx-auto lg:max-w-[600px] lg:w-full lg:pt-4 lg:pb-12">
       <RouterProvider router={router} />
     </div>
   );
