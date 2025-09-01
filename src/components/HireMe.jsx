14692a2a7f536fc63abe281d79c40d462a573276
AbalJerind <abaljerinda@gmail.com>
2025-09-01 16:55:15 +0700

    Style(HireMe): re-adjust hover state and remove div

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -2,13 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function HireMe() {
   return (
-    <div className="flex h-[47px] w-[98px] items-center justify-center rounded-lg transition duration-500 hover:bg-[#EBEBEB] md:h-[52px] md:w-[108px] dark:hover:bg-[#1C1C1C]">
-      <Link to="/contact">
-        <button className="flex cursor-pointer items-center rounded-md bg-[#141414] p-2 text-xs font-bold text-white md:text-sm dark:bg-[#383838]">
-          <img src="../images/Add.svg" alt="Add Icon" className="size-5" />
-          <span className="pl-1">Hire Me</span>
-        </button>
-      </Link>
-    </div>
+    <Link to="/contact">
+      <button className="flex cursor-pointer items-center rounded-lg bg-[#141414] p-2 text-xs font-bold text-white duration-300 hover:shadow-xl md:text-sm dark:bg-[#383838]">
+        <img src="../images/Add.svg" alt="Add Icon" className="size-5" />
+        <span className="pl-1">Hire Me</span>
+      </button>
+    </Link>
   );
 }
