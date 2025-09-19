e08d6b7cbbae401cb6070cf1941147296f221512
Abaljerind <abaljerinda@gmail.com>
2025-09-19 15:46:29 +0700

    Style(MySkills): adjust styles for hover state for each skill
    Receive props icon and color from About component
    Adjust group, transition property, duration and hover state to <div>
    Adjust cursor, font size for mobile and md breakpoint, display, align-items, justify-content and gap to <p>
    Add <span> with prop icon as value
    Adjust font size, transition property, duration, group-hover state, hover state, and font size for md and lg breakpoint to <span>

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,7 +1,13 @@
-export default function MySkills({ skill }) {
+export default function MySkills({ skill, icon, color }) {
   return (
-    <div className="rounded-full bg-white px-5 py-2 shadow-sm dark:bg-[#373737]">
-      <p className="text-sm font-semibold text-[#393939] md:text-[16px] dark:text-white">
+    <div className="group rounded-full bg-white px-5 py-2 shadow-sm transition-all duration-300 hover:scale-110 dark:bg-[#373737]">
+      <p className="flex cursor-pointer items-center justify-center gap-2 font-semibold text-[#393939] md:text-lg dark:text-white">
+        <span
+          className="text-xl transition duration-300 group-hover:text-[var(--hover-color)] hover:scale-125 md:text-2xl lg:text-3xl"
+          style={{ "--hover-color": color }}
+        >
+          {icon}
+        </span>
         {skill}
       </p>
     </div>
