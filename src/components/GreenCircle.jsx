ddbda8582e1ba509dc9d40863205f21a50866c2c
Abaljerind <abaljerinda@gmail.com>
2025-09-19 15:58:19 +0700

    Style(GreenCircle): adjust animation to <div> for green circle

--- src/components/GreenCircle.jsx
+++ src/components/GreenCircle.jsx
@@ -3,7 +3,7 @@ export default function GreenCircle() {
     <div className="flex items-center rounded-full bg-[#DFF7EC] lg:py-0.5 lg:pr-3 lg:pl-0.5 dark:bg-[#223A2F]">
       {/* Lingkaran Hijau */}
       <div className="flex h-6 w-6 items-center justify-center rounded-full bg-[#DFF7EC] dark:bg-[#223A2F]">
-        <div className="h-1.5 w-1.5 rounded-full bg-[#28C780] lg:h-[7px] lg:w-[7px]"></div>
+        <div className="h-1.5 w-1.5 animate-pulse rounded-full bg-[#28C780] lg:h-[7px] lg:w-[7px]"></div>
       </div>
 
       {/* Teks (Hanya Muncul di Layar ≥ 1024px) */}
