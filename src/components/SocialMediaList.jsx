8e5e5e35ab5794683a8ec2f7d3f8b2ac11631f47
Abaljerind <abaljerinda@gmail.com>
2025-10-03 11:02:12 +0700

    Refactor(SocialMediaList): Change <img> to icon from react-icons
    Receive props color from Footer component
    Add <span> with social.img as value for icon
    Delete some styles from <div> and <a>
    Re-adjust some styles to <div> and <a>

--- src/components/SocialMediaList.jsx
+++ src/components/SocialMediaList.jsx
@@ -1,17 +1,18 @@
-export default function SocialMediaList({ social }) {
+export default function SocialMediaList({ social, color }) {
   return (
-    <div className="flex h-11 w-11 cursor-pointer items-center justify-center rounded-full transition duration-500 hover:bg-[#EBEBEB] dark:duration-initial dark:hover:bg-[#1C1C1C]">
+    <div className="group flex h-11 w-11 cursor-pointer items-center justify-center rounded-full transition duration-500 hover:bg-[#e4e4e4] dark:duration-initial dark:hover:bg-[#414141]">
       <a
-        className="flex h-9 w-9 items-center justify-center rounded-full border border-[#E9E9E9] bg-white dark:border dark:border-[#212121] dark:bg-[#212121] dark:hover:border dark:hover:border-white/20"
+        className="flex h-9 w-9 items-center justify-center rounded-full bg-white text-lg text-white shadow-sm"
         href={social.href}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img
-          src={social.img}
-          alt={social.name}
-          className={`size-4 md:size-5 dark:brightness-0 dark:invert ${social.name === "FrontendMentor" ? "dark:brightness-100 dark:invert-0" : ""} `}
-        />
+        <span
+          className="text-lg text-gray-700 transition duration-300 group-hover:text-[var(--hover-color)]"
+          style={{ "--hover-color": color }}
+        >
+          {social.img}
+        </span>
       </a>
     </div>
   );
