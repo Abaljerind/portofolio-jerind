143345bc5e06804eb3c5f8a1995f466140745d98
Abaljerind <abaljerinda@gmail.com>
2025-10-03 08:32:36 +0700

    Refactor(ViewAll): Add styles and replace <img> to icon from react-icons
    Add gap to <button>
    Add brightness and invert in dark mode to <BsArrowRight>

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,17 +1,14 @@
+import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 export default function ViewAll() {
   return (
     <Link to={"/project"}>
-      <button className="flex w-full scale-90 cursor-pointer items-center justify-between rounded-lg border-2 border-[#EBEBEB] bg-white p-2.5 duration-300 hover:shadow-lg md:text-lg dark:border-none dark:bg-[#373737]">
+      <button className="flex w-full scale-90 cursor-pointer items-center justify-between gap-1 rounded-lg border-2 border-[#EBEBEB] bg-white p-2.5 duration-300 hover:shadow-lg md:text-lg dark:border-none dark:bg-[#373737]">
         <span className="text-sm font-bold text-[#373737] dark:text-white">
           View All
         </span>
-        <img
-          src="../images/RightArrow.svg"
-          alt="icon right arrow"
-          className="size-[14px] scale-75 dark:brightness-0 dark:invert"
-        />
+        <BsArrowRight className="dark:brightness-0 dark:invert" />
       </button>
     </Link>
   );
