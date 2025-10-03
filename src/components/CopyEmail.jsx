f9842c66990bf36aabf84ceabcf4156e8371b72d
Abaljerind <abaljerinda@gmail.com>
2025-10-03 10:36:13 +0700

    Refactor(CopyEmail): Change <img> to icon from react-icons
    Add font size, text color for dark mode to the icon

--- src/components/CopyEmail.jsx
+++ src/components/CopyEmail.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
+import { MdOutlineContentCopy } from "react-icons/md";
 
 export default function CopyEmail() {
-  //   Coba buat juga agar saat buttonText nya berganti ada animasi nya
-
   const [email] = useState("abaljerinda@gmail.com");
   const [buttonText, setButtonText] = useState("Copy Email");
   const [copyText, setCopyText] = useState(false);
@@ -26,11 +25,7 @@ export default function CopyEmail() {
       className={`flex w-28 cursor-pointer items-center rounded-md border-2 border-slate-300/50 bg-white p-2 text-xs font-bold duration-300 hover:shadow-lg dark:border-slate-300/10 dark:bg-[#212121] ${!copyText ? "text-[#444444] dark:text-white" : "text-[#737373]"} md:w-fit md:text-sm`}
       onClick={copyEmail}
     >
-      <img
-        src="../images/Copy.svg"
-        alt="copy icon"
-        className="size-4 opacity-50 dark:brightness-0 dark:invert"
-      />
+      <MdOutlineContentCopy className="text-lg dark:text-white" />
       <span className="pl-1">{buttonText}</span>
     </button>
   );
