0afd57db1a538a0a6291620a24f4628db5116141
Abaljerind <abaljerinda@gmail.com>
2025-10-03 11:05:32 +0700

    Refactor(HireMe): Change <img> to icon from react-icons

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -1,10 +1,11 @@
+import { IoIosAddCircleOutline } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 export default function HireMe() {
   return (
     <Link to="/contact">
       <button className="flex cursor-pointer items-center rounded-lg bg-[#141414] p-2 text-xs font-bold text-white duration-300 hover:shadow-xl md:text-sm dark:bg-[#383838]">
-        <img src="../images/Add.svg" alt="Add Icon" className="size-5" />
+        <IoIosAddCircleOutline className="text-lg" />
         <span className="pl-1">Hire Me</span>
       </button>
     </Link>
