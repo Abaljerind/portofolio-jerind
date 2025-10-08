3e1e2928ae61b6070950f17b9c289a9ac3fd1169
Abaljerind <abaljerinda@gmail.com>
2025-10-08 14:17:54 +0700

    Feat(ProjectCards): add 3 new projects
    Add bookmark landing page, browser extensions ui and easybank landing page

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -3,6 +3,24 @@ import { useLocation } from "react-router-dom";
 export default function ProjectCards() {
   const projectList = [
     {
+      name: "Browser Extensions Manager UI",
+      image: "../images/projects/browser-extensions.webp",
+      tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite, React Context, JSON",
+      href: "https://browser-extensions-manager-ui-jerind.netlify.app/",
+    },
+    {
+      name: "Bookmark Landing Page",
+      image: "../images/projects/bookmark-landing-page.webp",
+      tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite",
+      href: "https://bookmark-landing-page-one-coral.vercel.app/",
+    },
+    {
+      name: "Easybank Landing Page",
+      image: "../images/projects/easybank-landing-page.webp",
+      tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite",
+      href: "https://easybank-landing-page-peach-seven.vercel.app/",
+    },
+    {
       name: "Rest Countries API",
       image: "../images/projects/rest-countries-api.webp",
       tech: "HTML, TailwindCSS, JavaScript, NPM, REST API, React JS, Vite, React Context, React Router",
