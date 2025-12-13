17c0312d32f1c6bec6e0ec901e2edda377dbcc35
Abaljerind <abaljerinda@gmail.com>
2025-12-13 11:54:58 +0700

    Fix styling name

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -3,7 +3,7 @@ export default function MySkills({ skill, icon, color }) {
     <div className="group rounded-full bg-white px-5 py-2 shadow-sm transition-all duration-300 hover:scale-110 dark:bg-[#373737]">
       <p className="flex cursor-pointer items-center justify-center gap-2 font-semibold text-[#393939] md:text-lg dark:text-white">
         <span
-          className="text-xl transition duration-300 group-hover:text-[var(--hover-color)] hover:scale-125 md:text-2xl lg:text-3xl"
+          className="text-xl transition duration-300 group-hover:text-(--hover-color) hover:scale-125 md:text-2xl lg:text-3xl"
           style={{ "--hover-color": color }}
         >
           {icon}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ export default function NavBar() {
 
   return (
     <>
-      <nav className="flex h-16 items-center gap-1 rounded-xl bg-white pr-[10px] pl-4 shadow-lg shadow-black/5 lg:pl-6 dark:border dark:border-[#3D3D3D] dark:bg-[#212121]">
+      <nav className="flex h-16 items-center gap-1 rounded-xl bg-white pr-2.5 pl-4 shadow-lg shadow-black/5 lg:pl-6 dark:border dark:border-[#3D3D3D] dark:bg-[#212121]">
         <section className="left-side w-full">
           <NavItems />
         </section>
