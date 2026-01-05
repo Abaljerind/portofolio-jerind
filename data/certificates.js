0e4ba99822fe7436df76fd836fbebf0f2375e50e
Abaljerind <abaljerinda@gmail.com>
2026-01-05 11:51:56 +0700

    Feat(certificates): create file certificates for storing data of my certificates

---
+++ data/certificates.js
@@ -0,0 +1,61 @@
+export const certifications = {
+  web: [
+    {
+      title: "GIT Advanced",
+      issued: "Issued February 2023",
+      provider: "Codepolitan",
+      image: "../images/certificates/git.webp",
+    },
+    {
+      title: "Basic of HTML",
+      issued: "Issued March 2023",
+      provider: "Codepolitan",
+      image: "../images/certificates/basic-html.webp",
+    },
+    {
+      title: "Basic of CSS",
+      issued: "Issued March 2023",
+      provider: "Codepolitan",
+      image: "../images/certificates/basic-css.webp",
+    },
+    {
+      title: "JavaScript Course",
+      issued: "Issued March 2023",
+      provider: "Codepolitan",
+      image: "../images/certificates/js-course.webp",
+    },
+    {
+      title: "AJAX & Web API Course",
+      issued: "Issued May 2023",
+      provider: "Codepolitan",
+      image: "../images/certificates/ajax-and-web-api-course.webp",
+    },
+    {
+      title: "Basic of Web Programming",
+      issued: "Issued March 2024",
+      provider: "Dicoding Indonesia",
+      image: "../images/certificates/basic-web-programming.webp",
+    },
+    {
+      title: "Basic of JavaScript",
+      issued: "Issued April 2024",
+      provider: "Dicoding Indonesia",
+      image: "../images/certificates/basic-javascript.webp",
+    },
+    {
+      title: "React JS Course",
+      issued: "Issued January 2025",
+      provider: "Codepolitan",
+      image: "../images/certificates/react-js-course.webp",
+    },
+  ],
+
+  uiux: [
+    {
+      title: "UI/UX & Product Management Bootcamp",
+      issued: "Issued February 2025",
+      provider: "HariSenin",
+      image: "../images/certificates/ui-ux.webp",
+    },
+  ],
+};
