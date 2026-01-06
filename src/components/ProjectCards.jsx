608739c528c432ec2c2b1436a8ce21bce144ad71
Abaljerind <abaljerinda@gmail.com>
2026-01-06 11:23:50 +0700

    Feat(ProjectCards): re-create card for projects list
    Comment old codes of project cards
    Create new UI for project cards for mobile width

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -9,8 +9,8 @@ export default function ProjectCards() {
 
   return (
     <>
-      <div className="projects mt-1 flex flex-col gap-4 rounded-xl p-1.5">
-        {filteredProjectList.map((item, index) => {
+      <section className="projects mt-1 grid gap-8 rounded-xl p-1.5">
+        {/* {filteredProjectList.map((item, index) => {
           return (
             <div
               className="rounded-xl bg-white p-4 shadow-sm dark:border dark:border-[#3D3D3D] dark:bg-[#373737]"
@@ -36,8 +36,33 @@ export default function ProjectCards() {
               </small>
             </div>
           );
+        })} */}
+        {filteredProjectList.map((item, index) => {
+          return (
+            <div
+              className="flex flex-col-reverse gap-4 rounded-xl bg-white shadow-md dark:bg-[#373737]"
+              key={index + 1}
+            >
+              <div className="space-y-2 p-2.5">
+                <h4 className="cursor-pointer font-semibold text-[#373737] md:text-lg dark:text-white">
+                  {item.name}
+                </h4>
+                <p className="text-sm font-medium text-[#858585] md:text-sm dark:text-[#C0C0C0]">
+                  {item.about}
+                </p>
+                <button className="mt-2 w-full cursor-pointer rounded-md bg-[#373737] px-3 py-2 font-semibold text-white dark:bg-white dark:text-[#373737]">
+                  View Details
+                </button>
+              </div>
+              <img
+                src={item.image}
+                alt={item.name}
+                className="mx-auto w-full rounded-t-lg object-cover"
+              />
+            </div>
+          );
         })}
-      </div>
+      </section>
     </>
   );
 }
