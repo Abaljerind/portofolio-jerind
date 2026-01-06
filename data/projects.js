ee423e221c7e507a711644f38e4a746d928833a0
Abaljerind <abaljerinda@gmail.com>
2026-01-06 11:01:52 +0700

    Add property about

--- data/projects.js
+++ data/projects.js
@@ -4,53 +4,71 @@ export const projectList = [
     image: "../images/projects/browser-extensions.webp",
     tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite, React Context, JSON",
     href: "https://browser-extensions-manager-ui-jerind.netlify.app/",
+    about:
+      "A responsive UI for managing browser extensions, built as a solution to a Frontend Mentor challenge. All data is dynamically loaded from a local data.json file.",
   },
   {
     name: "Bookmark Landing Page",
     image: "../images/projects/bookmark-landing-page.webp",
     tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite",
     href: "https://bookmark-landing-page-one-coral.vercel.app/",
+    about:
+      "A clean and responsive landing page built with React JS (Vite) and Tailwind CSS, inspired by modern bookmark manager apps.",
   },
   {
     name: "Easybank Landing Page",
     image: "../images/projects/easybank-landing-page.webp",
     tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite",
     href: "https://easybank-landing-page-peach-seven.vercel.app/",
+    about:
+      "A clean and responsive landing page built with React JS (Vite) and Tailwind CSS, inspired by modern banking UI designs.",
   },
   {
     name: "Rest Countries API",
     image: "../images/projects/rest-countries-api.webp",
     tech: "HTML, TailwindCSS, JavaScript, NPM, REST API, React JS, Vite, React Context, React Router",
     href: "https://rest-countries-api-three-neon.vercel.app/",
+    about:
+      "A frontend web application that interacts with the REST Countries API to display useful information about countries around the world.",
   },
   {
     name: "NFT Marketplace Design",
     image: "../images/projects/cryptobase.webp",
     tech: "HTML, TailwindCSS, Vite, React JS, NPM, AXIOS, REST API, Firebase",
     href: "https://cryptobase-010325.web.app/",
+    about:
+      "A crypto watchlist web application built with React JS (Vite) and Tailwind CSS, featuring Firebase authentication and a Firestore database.",
   },
   {
     name: "Product List with Cart",
     image: "../images/projects/product-list-cart.webp",
     tech: "HTML, TailwindCSS, Vite, React JS, NPM, JSON",
     href: "https://product-list-with-card-chi.vercel.app/",
+    about:
+      "A simple React app built with React JS (Vite) to display a list of products using a JSON file as the data source.",
   },
   {
     name: "Advice Generator",
     image: "../images/projects/advice-generator.webp",
     tech: "HTML, TailwindCSS, Vite, React JS, NPM, REST API",
     href: "https://advice-generator-app-chi-six.vercel.app/",
+    about:
+      "A simple and interactive advice generator application built with React JS. This project fetches random advice using an API.",
   },
   {
     name: "Bookshelf Apps",
     image: "../images/projects/bookshelf-apps.webp",
     tech: "HTML, CSS, JavaScript, JSON, Local Storage",
     href: "https://bookshelf-apps-sable.vercel.app/",
+    about:
+      "A bookshelf web application that allows users to manage books they have read or plan to read. The application utilizes Web Storage (Local Storage) to persist book data directly in the browser.",
   },
   {
     name: "Todo Apps",
     image: "../images/projects/todo-apps.webp",
     tech: "HTML, CSS, JavaScript, JSON, Local Storage",
     href: "https://todo-apps-seven.vercel.app/",
+    about:
+      "This project demonstrates the results of my hands-on learning in vanilla JavaScript, built entirely using vanilla JavaScript and vanilla CSS without external libraries or frameworks.",
   },
 ];
