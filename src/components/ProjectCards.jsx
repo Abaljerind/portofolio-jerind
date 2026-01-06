0c075193ca55b1bea11ec842240231cf880803c2
Abaljerind <abaljerinda@gmail.com>
2026-01-06 10:36:53 +0700

    Feat(ProjectCards): import variable projectList from projects.js
    Create variable projectsList to store projectList
    Change projectList variable name to projectsList

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,66 +1,11 @@
 import { useLocation } from "react-router-dom";
+import { projectList } from "../../data/projects";
 
 export default function ProjectCards() {
-  const projectList = [
-    {
-      name: "Browser Extensions Manager UI",
-      image: "../images/projects/browser-extensions.webp",
-      tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite, React Context, JSON",
-      href: "https://browser-extensions-manager-ui-jerind.netlify.app/",
-    },
-    {
-      name: "Bookmark Landing Page",
-      image: "../images/projects/bookmark-landing-page.webp",
-      tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite",
-      href: "https://bookmark-landing-page-one-coral.vercel.app/",
-    },
-    {
-      name: "Easybank Landing Page",
-      image: "../images/projects/easybank-landing-page.webp",
-      tech: "HTML, TailwindCSS, JavaScript, NPM, React JS, Vite",
-      href: "https://easybank-landing-page-peach-seven.vercel.app/",
-    },
-    {
-      name: "Rest Countries API",
-      image: "../images/projects/rest-countries-api.webp",
-      tech: "HTML, TailwindCSS, JavaScript, NPM, REST API, React JS, Vite, React Context, React Router",
-      href: "https://rest-countries-api-three-neon.vercel.app/",
-    },
-    {
-      name: "NFT Marketplace Design",
-      image: "../images/projects/cryptobase.webp",
-      tech: "HTML, TailwindCSS, Vite, React JS, NPM, AXIOS, REST API, Firebase",
-      href: "https://cryptobase-010325.web.app/",
-    },
-    {
-      name: "Product List with Cart",
-      image: "../images/projects/product-list-cart.webp",
-      tech: "HTML, TailwindCSS, Vite, React JS, NPM, JSON",
-      href: "https://product-list-with-card-chi.vercel.app/",
-    },
-    {
-      name: "Advice Generator",
-      image: "../images/projects/advice-generator.webp",
-      tech: "HTML, TailwindCSS, Vite, React JS, NPM, REST API",
-      href: "https://advice-generator-app-chi-six.vercel.app/",
-    },
-    {
-      name: "Bookshelf Apps",
-      image: "../images/projects/bookshelf-apps.webp",
-      tech: "HTML, CSS, JavaScript, JSON, Local Storage",
-      href: "https://bookshelf-apps-sable.vercel.app/",
-    },
-    {
-      name: "Todo Apps",
-      image: "../images/projects/todo-apps.webp",
-      tech: "HTML, CSS, JavaScript, JSON, Local Storage",
-      href: "https://todo-apps-seven.vercel.app/",
-    },
-  ];
-
+  const projectsList = projectList;
   const location = useLocation();
   const filteredProjectList =
-    location.pathname === "/project" ? projectList : projectList.slice(0, 3);
+    location.pathname === "/project" ? projectsList : projectsList.slice(0, 3);
 
   return (
     <>
