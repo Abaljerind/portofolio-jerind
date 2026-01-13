a41dcce766e5754203f1d14569141d11dd5fe4c7
Abaljerind <abaljerinda@gmail.com>
2026-01-13 10:53:18 +0700

    Feat(ProjectCards): Change tag <button> view details to be <Link>
    Add attr to to tag <Link>
    Change style for both buttons to be a custom css

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { projectList } from "../../data/projects";
 
 export default function ProjectCards() {
@@ -10,33 +10,6 @@ export default function ProjectCards() {
   return (
     <>
       <section className="projects mt-1 grid gap-8 rounded-xl p-1.5">
-        {/* {filteredProjectList.map((item, index) => {
-          return (
-            <div
-              className="rounded-xl bg-white p-4 shadow-sm dark:border dark:border-[#3D3D3D] dark:bg-[#373737]"
-              key={index + 1}
-            >
-              <a
-                className="block"
-                href={item.href}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="mb-3 rounded-xl"
-                />
-                <p className="cursor-pointer text-[16px] font-semibold text-[#373737] md:text-lg dark:text-white">
-                  {item.name}
-                </p>
-              </a>
-              <small className="text-xs font-medium text-[#858585] md:text-sm dark:text-[#C0C0C0]">
-                {item.tech}
-              </small>
-            </div>
-          );
-        })} */}
         {filteredProjectList.map((item, index) => {
           return (
             <div
@@ -50,12 +23,14 @@ export default function ProjectCards() {
                 <p className="text-sm font-medium text-[#858585] md:text-sm dark:text-[#C0C0C0]">
                   {item.about}
                 </p>
-                <div className="mt-4 flex gap-2.5">
-                  <button className="w-full cursor-pointer rounded-lg bg-[#373737] px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-[#373737]/90 lg:text-base dark:bg-white dark:text-[#373737] hover:dark:bg-white/90">
+                <div className="mt-4 flex gap-2.5 text-center">
+                  <Link to={`/project/${item.id}`} className="buttons-project">
                     View Details
-                  </button>
-                  <button className="w-full cursor-pointer rounded-lg bg-[#373737] px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-[#373737]/90 lg:text-base dark:bg-white dark:text-[#373737] hover:dark:bg-white/90">
-                    <a href={item.href}>Live Preview</a>
+                  </Link>
+                  <button className="buttons-project">
+                    <a href={item.href} target="_blank">
+                      Live Preview
+                    </a>
                   </button>
                 </div>
               </div>
