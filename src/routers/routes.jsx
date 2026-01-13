7deaed05cfcd0db765470807aedc7a64aad4a730
Abaljerind <abaljerinda@gmail.com>
2026-01-13 10:54:48 +0700

    Feat(routes): add new path for detail project page

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -5,6 +5,7 @@ import About from "../pages/About";
 import Project from "../pages/Project";
 import Contact from "../pages/Contact";
 import Certificate from "../pages/MyCertificate";
+import ProjectDetail from "../pages/ProjectDetail";
 
 export const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
       {
         path: "/project",
         element: <Project />,
+      },
+      {
+        path: "/project/:id",
+        element: <ProjectDetail />,
       },
       {
         path: "/contact",
