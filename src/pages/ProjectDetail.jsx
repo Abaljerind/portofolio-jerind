7c14df3014432653083ca6f2c91959758ec8784f
Abaljerind <abaljerinda@gmail.com>
2026-01-15 15:09:22 +0700

    Style(ProjectDetail): add style for breakpoint lg & xl
    Change button name for github repo
    Delete comment section

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -10,10 +10,6 @@ const ProjectDetail = () => {
     (project) => project.id === Number(params.id),
   );
 
-  /*
-    TODO:
-    ! kasih style untuk tablet dan desktop width
-     */
   return (
     <section className="">
       <ScrollRestoration />
@@ -26,26 +22,29 @@ const ProjectDetail = () => {
           alt={detail?.name}
           className="mt-2 mb-4 rounded-md object-cover"
         />
-        <div className="space-y-2 p-2 dark:text-white">
-          <h1 className="text-lg font-bold">{detail?.name}</h1>
-          <p className="text-sm">{detail?.about}</p>
+        <div className="space-y-2 p-2 lg:p-4 dark:text-white">
+          <h1 className="text-lg font-bold xl:text-xl">{detail?.name}</h1>
+          <p className="text-sm xl:text-base">{detail?.about}</p>
         </div>
       </header>
 
-      <main className="space-y-4 p-2 dark:text-white">
+      <main className="space-y-4 p-2 lg:p-4 lg:pt-0 dark:text-white">
         <div className="space-y-2">
-          <h2 className="text-lg font-bold">My Role</h2>
-          <p className="text-sm">{detail?.myRole}</p>
+          <h2 className="text-lg font-bold xl:text-xl">My Role</h2>
+          <p className="text-sm xl:text-base">{detail?.myRole}</p>
         </div>
 
         <div className="space-y-2">
-          <h2 className="text-lg font-bold">Key Features</h2>
+          <h2 className="text-lg font-bold xl:text-xl">Key Features</h2>
           <ul className="space-y-1.5">
             {detail?.features.map((item, index) => {
               return (
-                <li className="flex items-start gap-2 text-sm" key={index + 1}>
+                <li
+                  className="flex items-start gap-2 text-sm xl:text-base"
+                  key={index + 1}
+                >
                   <span className="">
-                    <MdCheckBoxOutlineBlank className="mt-0.5 size-4 text-gray-400 dark:text-white" />
+                    <MdCheckBoxOutlineBlank className="mt-0.5 size-4 text-gray-400 xl:size-5 dark:text-white" />
                   </span>
                   {item}
                 </li>
@@ -55,7 +54,9 @@ const ProjectDetail = () => {
         </div>
 
         <div className="mb-6">
-          <h2 className="mb-4 text-lg font-bold">Technologies Used</h2>
+          <h2 className="mb-4 text-lg font-bold xl:text-xl">
+            Technologies Used
+          </h2>
           <ul className="flex flex-wrap gap-3">
             {detail?.tech.map((item, index) => {
               return (
@@ -73,7 +74,7 @@ const ProjectDetail = () => {
         <div className="mt-4 flex gap-2.5">
           <button className="buttons-project">
             <a href={detail?.repo} target="_blank">
-              View GitHub Repository
+              Project Repository
             </a>
           </button>
           <button className="buttons-project">
