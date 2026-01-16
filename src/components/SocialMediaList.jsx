9dccf929f89fbd38289a16cc3359ca59ba16cd07
Abaljerind <abaljerinda@gmail.com>
2026-01-16 11:49:14 +0700

    Refactor(SocialMediaList): add animation with framer motion
    Import motion
    Create variable fadeRight
    Change tag <div> to tag <motion.div>
    Fix utility tailwind for tag <span>

--- src/components/SocialMediaList.jsx
+++ src/components/SocialMediaList.jsx
@@ -1,6 +1,24 @@
-export default function SocialMediaList({ social, color }) {
+import { motion } from "motion/react";
+
+export default function SocialMediaList({ social, color, index }) {
+  const fadeRight = {
+    hidden: { opacity: 0, x: 200 },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
-    <div className="group flex h-11 w-11 cursor-pointer items-center justify-center rounded-full transition duration-500 hover:bg-[#e4e4e4] dark:duration-initial dark:hover:bg-[#414141]">
+    <motion.div
+      variants={fadeRight}
+      initial="hidden"
+      whileInView="visible"
+      transition={{
+        ease: "easeIn",
+        duration: 0.8,
+        delay: index * 0.2,
+        staggerChildren: 0.1,
+      }}
+      className="group flex h-11 w-11 cursor-pointer items-center justify-center rounded-full transition duration-500 hover:bg-[#e4e4e4] dark:duration-initial dark:hover:bg-[#414141]"
+    >
       <a
         className="flex h-9 w-9 items-center justify-center rounded-full bg-white text-lg text-white shadow-sm"
         href={social.href}
@@ -8,12 +26,12 @@ export default function SocialMediaList({ social, color }) {
         rel="noopener noreferrer"
       >
         <span
-          className="text-lg text-gray-700 transition duration-300 group-hover:text-[var(--hover-color)]"
+          className="text-lg text-gray-700 transition duration-300 group-hover:text-(--hover-color)"
           style={{ "--hover-color": color }}
         >
           {social.img}
         </span>
       </a>
-    </div>
+    </motion.div>
   );
 }
