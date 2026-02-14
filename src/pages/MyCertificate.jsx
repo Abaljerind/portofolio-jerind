cea33e988e1b7852e20661230fc059973728d376
Abaljerind <abaljerinda@gmail.com>
2026-02-14 10:50:24 +0700

    Feat(MyCertificate): add animation with framer motion

--- src/pages/MyCertificate.jsx
+++ src/pages/MyCertificate.jsx
@@ -5,6 +5,7 @@ import GrayCircle from "../components/GrayCircle";
 import { IoClose } from "react-icons/io5";
 import { PiMedalLight } from "react-icons/pi";
 import { certifications } from "../../data/certificates";
+import { AnimatePresence, motion } from "motion/react";
 
 const MyCertificate = () => {
   const [activeTab, setActiveTab] = useState("web");
@@ -12,59 +13,108 @@ const MyCertificate = () => {
 
   const certificates = certifications;
 
+  const container = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.12,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.6, ease: "easeOut" },
+    },
+  };
+
   return (
     <>
       <section className="px-2">
         {/* header */}
-        <header className="title-about flex items-center justify-between px-4 py-5 lg:px-6">
-          <div className="flex items-center justify-between">
+        <motion.header
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          className="title-about flex items-center justify-between px-4 py-5 lg:px-6"
+        >
+          <motion.div
+            variants={item}
+            className="flex items-center justify-between"
+          >
             <GrayCircle />
             <p className="text-lg font-semibold text-[#666666] md:text-xl dark:text-[#C0C0C0]">
               Certificate
             </p>
-          </div>
-        </header>
+          </motion.div>
+        </motion.header>
 
-        <section className="my-projects flex flex-col px-4 lg:px-6">
-          <h2 className="mb-4 text-3xl font-bold text-[#373737] md:mt-4 md:mb-6 md:text-4xl dark:text-white">
+        <motion.section
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          className="my-projects flex flex-col px-4 lg:px-6"
+        >
+          <motion.h2
+            variants={item}
+            className="mb-4 text-3xl font-bold text-[#373737] md:mt-4 md:mb-6 md:text-4xl dark:text-white"
+          >
             My Certifications
-          </h2>
-          <p className="mb-4 text-[16px] leading-7 tracking-tight text-pretty text-[#666666] md:text-lg dark:text-[#C0C0C0]">
+          </motion.h2>
+          <motion.p
+            variants={item}
+            className="mb-4 text-[16px] leading-7 tracking-tight text-pretty text-[#666666] md:text-lg dark:text-[#C0C0C0]"
+          >
             A showcase of my continuous learning journey. Each certificate
             represents dedication to mastering modern web development and design
             skills, ensuring I deliver innovative, reliable, and future-ready
             digital solutions.
-          </p>
-        </section>
+          </motion.p>
+        </motion.section>
         {/* ./ header */}
 
         {/* content */}
         <main>
           {/* tab */}
-          <nav
+          <motion.nav
+            variants={container}
+            initial="hidden"
+            animate="visible"
             className="mx-auto flex border-b border-gray-400"
             aria-label="Certification categories"
           >
-            <button
+            <motion.button
+              variants={item}
               className={`flex-1 p-3 text-center ${activeTab === "web" ? "font-semibold text-[#373737] transition-all duration-300 dark:text-white" : "text-[#666]"} cursor-pointer md:text-lg`}
               onClick={() => setActiveTab("web")}
             >
               Web Development
-            </button>
-            <button
+            </motion.button>
+            <motion.button
+              variants={item}
               className={`flex-1 p-3 text-center ${activeTab === "uiux" ? "font-semibold text-[#373737] transition-all duration-300 dark:text-white" : "text-[#666]"} cursor-pointer md:text-lg`}
               onClick={() => setActiveTab("uiux")}
             >
               UI/UX Design
-            </button>
-          </nav>
+            </motion.button>
+          </motion.nav>
           {/* ./ tab */}
 
           {/* tab content */}
-          <section className="mt-4 flex-1 space-y-5 rounded-xl bg-[#F7F7F7] px-3 py-5 md:p-6 dark:bg-[#2C2C2C]">
+          <motion.section
+            variants={container}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.3 }}
+            className="mt-4 flex-1 space-y-5 rounded-xl bg-[#F7F7F7] px-3 py-5 md:p-6 dark:bg-[#2C2C2C]"
+          >
             {certificates[activeTab].map((cert, idx) => {
               return (
-                <article
+                <motion.article
+                  variants={item}
                   className="flex items-end justify-between gap-1.5"
                   key={idx + 1}
                 >
@@ -85,64 +135,83 @@ const MyCertificate = () => {
                   >
                     View Credential
                   </button>
-                </article>
+                </motion.article>
               );
             })}
-          </section>
+          </motion.section>
           {/* ./ tab content */}
 
           {/* modal / view certificate */}
-          {selectedCert && (
-            <div
-              className="fixed inset-0 z-40 flex items-center justify-center bg-black/70"
-              role="dialog"
-              aria-modal="true"
-              aria-labelledby="cert-title"
-            >
-              <div className="w-[90%] max-w-3xl space-y-4 rounded-lg bg-white p-6 md:w-4/5 xl:w-3/4 xl:max-w-2xl">
-                {/* title & close button */}
-                <div className="mb-4 flex items-center justify-between">
-                  <div className="flex items-center justify-between gap-4">
-                    <PiMedalLight className="text-2xl text-violet-600" />
-                    <h2
-                      id="cert-title"
-                      className="font-semibold text-[#373737]"
+          <AnimatePresence>
+            {selectedCert && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.15, ease: "easeIn" }}
+                className="fixed inset-0 z-40 flex items-center justify-center bg-black/70"
+                role="dialog"
+                aria-modal="true"
+                aria-labelledby="cert-title"
+              >
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.95, y: 10 }}
+                  animate={{ opacity: 1, scale: 1, y: 0 }}
+                  exit={{
+                    opacity: 0,
+                    scale: 0.9,
+                    y: 20,
+                  }}
+                  transition={{
+                    duration: 0.18,
+                    ease: "easeIn",
+                  }}
+                  className="w-[90%] max-w-3xl space-y-4 rounded-lg bg-white p-6 md:w-4/5 xl:w-3/4 xl:max-w-2xl"
+                >
+                  {/* title & close button */}
+                  <div className="mb-4 flex items-center justify-between">
+                    <div className="flex items-center justify-between gap-4">
+                      <PiMedalLight className="text-2xl text-violet-600" />
+                      <h2
+                        id="cert-title"
+                        className="font-semibold text-[#373737]"
+                      >
+                        {selectedCert.title}
+                      </h2>
+                    </div>
+                    <button
+                      onClick={() => setSelectedCert(null)}
+                      className="cursor-pointer rounded-full text-2xl text-gray-700 transition-all duration-200 hover:bg-[#494848] hover:text-white"
+                      aria-label="close-modal"
                     >
-                      {selectedCert.title}
-                    </h2>
+                      <IoClose />
+                    </button>
                   </div>
-                  <button
-                    onClick={() => setSelectedCert(null)}
-                    className="cursor-pointer rounded-full text-2xl text-gray-700 transition-all duration-200 hover:bg-[#494848] hover:text-white"
-                    aria-label="close-modal"
-                  >
-                    <IoClose />
-                  </button>
-                </div>
-                {/* ./ title & close button */}
+                  {/* ./ title & close button */}
 
-                {/* certificate image */}
-                <figure>
-                  <img
-                    src={selectedCert.image}
-                    alt={selectedCert.title}
-                    className="w-full rounded-lg border-8 border-gray-800"
-                  />
+                  {/* certificate image */}
+                  <figure>
+                    <img
+                      src={selectedCert.image}
+                      alt={selectedCert.title}
+                      className="w-full rounded-lg border-8 border-gray-800"
+                    />
 
-                  <figcaption className="mt-4 space-y-0.5 text-center text-[#666666]">
-                    <p>
-                      Issued by{" "}
-                      <span className="font-semibold">
-                        {selectedCert.provider}
-                      </span>
-                    </p>
-                    <p className="text-xs">{selectedCert.issued.slice(-4)}</p>
-                  </figcaption>
-                </figure>
-                {/* ./ certificate image */}
-              </div>
-            </div>
-          )}
+                    <figcaption className="mt-4 space-y-0.5 text-center text-[#666666]">
+                      <p>
+                        Issued by{" "}
+                        <span className="font-semibold">
+                          {selectedCert.provider}
+                        </span>
+                      </p>
+                      <p className="text-xs">{selectedCert.issued.slice(-4)}</p>
+                    </figcaption>
+                  </figure>
+                  {/* ./ certificate image */}
+                </motion.div>
+              </motion.div>
+            )}
+          </AnimatePresence>
           {/* ./ modal / view certificate */}
 
           {/* ./ content */}
